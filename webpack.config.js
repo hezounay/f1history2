fd95a10dd5e616c6bf326edf8ba69a9cdfe43517
unknown <vanitashez@gmail.com>
2020-06-09 23:47:39 +0200

    admin fixed

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,9 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/js/app.js')
-    //.addEntry('page1', './assets/js/page1.js')
+    .addEntry('appadmin', './assets/css/bootstrap.min.css')
+    .addEntry('appadminjs', './assets/js/bootstrap.min.js')
+    
     //.addEntry('page2', './assets/js/page2.js')
     .addPlugin(new CopyPlugin( 
         { 
